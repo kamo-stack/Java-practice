d23357621ffe3aa1bae4e07d8f3ba9eabe7632a3
kamo-stack <kamoashley33@gmail.com>
2024-11-24 02:48:31 +0200

    first practice of Java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/* Write a Java program to print 'Hello' on screen and your name on a separate line.
+Expected Output :
+Hello
+Alexandra Abramov */
+
+public class Main{
+    public static void main(String[] args){
+        String message = "Hello";
+        String myName = "Ashley Kamo";
+        System.out.println(message);
+        System.out.println(myName);
+    }
+}
